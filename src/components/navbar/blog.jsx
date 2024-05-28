07a24a23a9ae877e6f0c7df9857439954efd3fa2
jsdeveloper0401 <wolfmaks85@gmail.com>
2024-05-28 18:40:44 +0500

    gdgfs

--- src/components/navbar/blog.jsx
+++ src/components/navbar/blog.jsx
@@ -1,9 +1,50 @@
+import "./blog.css";
+
 const Blog = () => {
+    const posts = [
+        {
+            title: "UI Interactions of the week",
+            date: "12 Feb 2019",
+            tags: "Express, Handlebars",
+            description:
+                "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+        },
+        {
+            title: "UI Interactions of the week",
+            date: "12 Feb 2019",
+            tags: "Express, Handlebars",
+            description:
+                "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+        },
+        {
+            title: "UI Interactions of the week",
+            date: "12 Feb 2019",
+            tags: "Express, Handlebars",
+            description:
+                "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+        },
+    ];
+
     return (
-        <div>
-            <h2>Blog</h2>
-            <p>Welcome to the Blog page.</p>
-        </div>
+        <section className="list">
+            <h1>Blog</h1>
+            <div className="boxes">
+                {posts.map((post, index) => (
+                    <article key={index} className="box">
+                        <h2 className="box__title">{post.title}</h2>
+                        <div className="data">
+                            <p>{post.date}</p>
+                            <div className="slash"></div>
+                            <h4>{post.tags}</h4>
+                        </div>
+                        <p className="box__description">{post.description}</p>
+                        {index < posts.length - 1 && (
+                            <div className="line"></div>
+                        )}
+                    </article>
+                ))}
+            </div>
+        </section>
     );
 };
 
